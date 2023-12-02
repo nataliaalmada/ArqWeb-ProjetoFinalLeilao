6a7403b643b53ae2a83ecac2f32ddd41ec4f9aaa
Natália Almada <almada.compsci@gmail.com>
2023-12-02 15:06:46 -0300

    criação de conexão em tempo real

--- Novo Projeto - deve ser o quarto já/bidding-system/bidding-system/bidding-system/server/index.js
+++ Novo Projeto - deve ser o quarto já/bidding-system/bidding-system/bidding-system/server/index.js
@@ -6,6 +6,34 @@ const app = express();
 
 const PORT = 4000;
 
+const http = require('http').Server(app);
+
+const cors = require('cors');
+
+const socketIO = require('socket.io')(http, {
+
+    cors: {
+
+        origin: "http://localhost:3000"
+
+    }
+
+});
+
+
+//Add this before the app.get() block
+
+socketIO.on('connection', (socket) => {
+
+    console.log(`⚡: ${socket.id} user just connected!`);
+
+    socket.on('disconnect', () => {
+
+      console.log('🔥: A user disconnected');
+
+    });
+
+});
 
 app.get('/api', (req, res) => {
 
