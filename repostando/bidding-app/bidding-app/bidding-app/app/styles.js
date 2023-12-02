baa044ef3e5874c0fb944fabde2d2b197506bc52
Natália Almada <almada.compsci@gmail.com>
2023-12-02 14:30:57 -0300

    terceira tentativa

---
+++ repostando/bidding-app/bidding-app/bidding-app/app/styles.js
@@ -0,0 +1,249 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+
+    addProductContainer: {
+
+        flex: 1,
+
+    },
+
+    productForm: {
+
+        width: "100%",
+
+        padding: 10,
+
+    },
+
+    formInput: {
+
+        borderWidth: 1,
+
+        padding: 15,
+
+        marginTop: 5,
+
+        marginBottom: 10,
+
+    },
+
+    addProductBtn: {
+
+        backgroundColor: "green",
+
+        padding: 15,
+
+    },
+
+    headerContainer: {
+
+        padding: 15,
+
+        flexDirection: "row",
+
+        justifyContent: "space-between",
+
+    },
+
+
+    mainContainer: {
+
+        flex: 1,
+
+        padding: 20,
+
+    },
+
+    loginContainer: {
+
+        flex: 1,
+
+        justifyContent: "center",
+
+        alignItems: "center",
+
+    },
+
+    heading: {
+
+        fontSize: 25,
+
+        fontWeight: "bold",
+
+        marginBottom: 20,
+
+    },
+
+    formContainer: {
+
+        width: "100%",
+
+        padding: 15,
+
+    },
+
+    input: {
+
+        borderWidth: 1,
+
+        paddingHorizontal: 10,
+
+        paddingVertical: 15,
+
+        marginBottom: 15,
+
+        borderRadius: 3,
+
+    },
+
+    formLabel: {
+
+        marginBottom: 3,
+
+    },
+
+    loginbutton: {
+
+        backgroundColor: "green",
+
+        width: 150,
+
+        padding: 15,
+
+        alignItems: "center",
+
+        borderRadius: 5,
+
+    },
+
+    loginbuttonText: {
+
+        color: "#fff",
+
+    },
+
+    modalContainer: {
+
+        width: "100%",
+
+        backgroundColor: "#FAF7F0",
+
+        position: "fixed",
+
+        bottom: 0,
+
+        height: 400,
+
+        padding: 20,
+
+        flex: 1,
+
+    },
+
+    modalHeader: {
+
+        fontSize: 24,
+
+        fontWeight: "bold",
+
+        textAlign: "center",
+
+        marginBottom: 20,
+
+    },
+
+    modalPrice: {
+
+        width: "100%",
+
+        borderWidth: 1,
+
+        padding: 12,
+
+    },
+
+    updateBidBtn: {
+
+        width: 200,
+
+        padding: 15,
+
+        backgroundColor: "green",
+
+        marginTop: 15,
+
+        borderRadius: 3,
+
+    },
+
+    bidContainer: {
+
+        flex: 1,
+
+        backgroundColor: "#fff",
+
+    },
+
+    header: {
+
+        fontSize: 24,
+
+        fontWeight: "bold",
+
+    },
+
+    mainContainer: {
+
+        flex: 1,
+
+        padding: 20,
+
+    },
+
+    productContainer: {
+
+        borderWidth: 1,
+
+        borderColor: "#B2B2B2",
+
+        padding: 20,
+
+        height: 280,
+
+        backgroundColor: "#fff",
+
+        marginBottom: 10,
+
+    },
+
+    image: {
+
+        width: "100%",
+
+        height: "70%",
+
+    },
+
+    productDetails: {
+
+        width: "100%",
+
+        height: "30%",
+
+        padding: 10,
+
+        alignItems: "center",
+
+    },
+
+    productName: {
+
+        fontSize: 16,
+
+        fontWeight: "bold",
+
+    },
+
+});
